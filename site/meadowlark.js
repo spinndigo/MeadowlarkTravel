b805e6bb1567d4addf9fc1e1ef045a5e09a5b0e1
Joseph Spinosa <spinosajoseph@gmail.com>
2023-09-15 11:44:43 -0400

    ch3 fortunes

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -1,3 +1,9 @@
+
+/*  ---------------------- MISC ----------------------------   */
+
+var fortunes = ['Conquer your fears or they will conquer you' , 'Rivers need springs' , 'Do not fear what you don\'t know' , 'You will have a pleasent surprise', 'Whenever possible, keep it simple'];
+
+/*  ---------------------- APP ----------------------------   */
 var express = require('express');
 var app = express();
 
@@ -8,12 +14,15 @@ app.set('view engine' , 'handlebars');
 
 app.set('port' , process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/' , function(req, res){
     res.render('home');
 });
 
 app.get('/about' , function(req, res){
-   res.render('about');
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+   res.render('about', {fortune: randomFortune});
 })
 
 app.use(function(req, res){
